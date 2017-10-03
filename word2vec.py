78047437c93b45653a5e6c0f34b75eb519b92674
jneo8 <james851212@gmail.com>
2017-10-04 00:30:32 +0800

    Add read txt data

--- word2vec.py
+++ word2vec.py
@@ -2,8 +2,8 @@
 
 import os
 import pandas as pd
-
 from utils import settings
+import jieba
 
 # logger
 from logger import logconf
@@ -13,8 +13,19 @@ logger = logconf.Logger(__name__)
 def read_data():
     """Read origin data use pandas."""
     source_list = [
-        '獸性老公吻上癮.txt'
+        '獸性老公吻上癮.txt',
+        '惡魔總裁的小妻子.txt',
     ]
+    source_list = [os.path.join(settings.SOURCE_DIR, s) for s in source_list]
 
-    source_list = [os]
+    list_ = []
+    for s in source_list:
+        logger.debug(s)
+        df_ = pd.read_csv(s, sep=" ", header=None, names=['text'])
+        list_.append(df_)
+    df = pd.concat(list_)
+    logger.debug(df.shape)
+
+if __name__ == '__main__':
+    read_data()
 
